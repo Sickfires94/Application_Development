74c545609a1275e6f872a2d8b24fc2be3ecc0ffd
Abdul Rehman Ansari <a.ansari.24487@khi.iba.edu.pk>
2024-10-01 11:06:24 +0500

    ListViewBuilder Implemented with API integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/NewList.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -36,32 +37,27 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
+    List<String> myProducts = ["hello", "hi"];
+    for (int i = 0; i < 100; i++)
+      myProducts.add("hi" + i.toString());
+
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times (new):',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
+        body:
+        NewList()
     );
+
   }
+
 }
+
