14b9536d1f66984f605ac48837e756b52da69744
Abdul Rehman Ansari <a.ansari.24487@khi.iba.edu.pk>
2024-10-29 10:18:22 +0500

    Bloc implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:first_app/ProductBloc.dart';
+import 'package:first_app/products.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,54 +17,67 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home'),
+      home: BlocProvider(
+        create: (context) => ProductBloc()..add(FetchProducts()),
+        child: ProductScreen()
+      )
+      //home: ProductsList(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
+class ProductScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times (new):',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      appBar: AppBar(title: const Text("Products")),
+      body: BlocBuilder<ProductBloc, ProductState>(
+          builder: (context, state){
+            if (state is ProductLoading){
+              return Center(child: CircularProgressIndicator(),);
+             // return const SkeletonList extends statelessWidget();
+            } else if (state is ProductLoaded){
+              List<Product> products = state.products;
+              return ProductList(products: products);
+            } else if (state is ProductError) {
+              return Center(child: Text(state.error));
+            }
+            return const Center(child: Text("Press button to fetch Products"));
+          })
     );
   }
 }
+
+
+class ProductList extends StatelessWidget{
+  final List<Product> products;
+  ProductList({required this.products});
+
+  Widget build(BuildContext context){
+    return ListView.builder(
+      // the number of items in the list
+        itemCount: products.length,
+
+        // display each item of the product list
+        itemBuilder: (context, index) {
+          return Card(
+            // In many cases, the key isn't mandatory
+            key: ValueKey(products[index]),
+            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      products[index].thumbnail),
+                ),
+                title: Text(products[index].title),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text((products[index].description), overflow: TextOverflow.ellipsis,),
+                  Text((products[index].price.toString()), style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold), ),
+                ],)
+            ),
+          );
+        });
+  }
+}
+
